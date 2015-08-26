e24a7acc1e5285f85b7691fb4da25600bef86dd9
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2015-08-26 13:17:00 -0700

    adds initial api server

---
+++ api/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var pg = require('pg')
+
+app.get('/', function(req, res) {
+    console.log('request received')
+    res.status(200).json({'ogewr': 'etwerwer'})
+});
+
+
+var server = app.listen(8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('HTTP listening at http://%s:%s', host, port);
+});
+
