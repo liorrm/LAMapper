93aa90575fc8a686a3420d0653e6bbe47610e38c
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2015-09-14 13:31:33 -0700

    adds controller-like initializer script for react

---
+++ client/public/application.jsx
@@ -0,0 +1,15 @@
+//file to initialize React component
+
+
+// console.log(React, React.render)
+React.render(
+    <Leaflet />, document.getElementById('app')
+);
+
+// console.log('hey')
+
+// application structure:
+
+// Application Component
+//   - Leaflet Component
+//   - Sidebar Component (eventually)
