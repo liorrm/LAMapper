bf2371e69e8c4c65f0a8280b74314a1c4b16c08d
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2015-09-14 15:54:44 -0700

    adds neighborhood model and first endpoint

--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
-var app = express();
 var express = require('express');
+var app = express();
 var pg = require('pg');
 var regionModel = require('./models/region');
+var neighborhoodModel = require('./models/neighborhood');
 
-app.get('/api/regions', function(req, res) {
+app.get('/regions', function(req, res) {
     console.log('request received');
     regionModel.selectAll(function(err, results) {
         if (err) {
@@ -14,8 +15,19 @@ app.get('/api/regions', function(req, res) {
     })
 });
 
+app.get('/neighborhoods', function(req, res) {
+    console.log('request received');
+    neighborhoodModel.selectAll(function(err, results) {
+        if (err) {
+            return res.status(500).json({'message': 'database error'})
+        } else {
+            return res.status(200).json(results);
+        }
+    })
+});
 
-var server = app.listen(8080, function () {
+
+var server = app.listen(8081, function () {
     var host = server.address().address;
     var port = server.address().port;
 
